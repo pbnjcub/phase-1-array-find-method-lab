59a05cc0d6d7c4f12bd03cf143df24a72298aec4
Paul Jung <pbnjcub@gmail.com>
2022-07-05 13:22:00 -0400

    commit tree 0921e72f84b179af98632d1d7c258eec756b7bea

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+let superbowlWin = function (record) {
+    let yearWin = null  
+    yearWin = record.find(record => record.result === "W")
+    if(yearWin === undefined) {
+        return yearWin
+    } else {
+        return yearWin.year
+    }
+}
